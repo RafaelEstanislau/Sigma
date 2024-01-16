5ada9a5f05595e4ce6995efc63e8ef215253280f
Rafael Estanislau <rafalevel100@hotmail.com>
2024-01-16 15:42:54 -0300

    trying cors

--- backend/server.ts
+++ backend/server.ts
@@ -14,13 +14,19 @@ const app = express();
 const cors = require("cors");
 
 // middleware
-const corsOptions = {
-    origin: "https://rafa-frontend-lyart.vercel.app/", // frontend URI (ReactJS)
-}
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    allowedHeaders: ["authorization", "Content-Type"],
+    exposedHeaders: ["authorization"],
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello');
